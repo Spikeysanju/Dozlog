4ca2fe958ff5612953c307fdf576c3f6a70fd609
Spikey Sanju <spikeysanju98@gmail.com>
2023-06-22 10:45:15 +0530

    add toast & fix style

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -5,6 +5,8 @@
 	import { OperationType } from '@dozerjs/dozer/lib/cjs/generated/protos/types_pb.js';
 	import ProductCard from '$lib/components/ProductCard.svelte';
 	import { enhance } from '$app/forms';
+	import EmptyState from '$lib/components/EmptyState.svelte';
+	import toast from 'svelte-french-toast';
 
 	let results: any = null;
 	let records: any = [];
@@ -221,21 +223,24 @@
 				return err;
 			});
 
-		alert('Checkout successful');
+		toast.success('Checkout successful');
 	}
 </script>
 
-<div class="h-screen">
+<div class="h-screen w-full">
 	<h2 class="text-xl font-bold text-gray-900 py-3">My Cart</h2>
 
-	<div class=" w-full grid grid-cols-2 gap-x-6 items-start justify-center">
-		<div
-			id="cart-items"
-			class="flex flex-col col-span-2 sm:col-span-1 w-full items-center justify-center gap-3 max-w-2xl"
-		>
-			{#if allCartItems.length === 0}
-				<p class="text-gray-900 text-sm">No items in cart</p>
-			{:else}
+	{#if allCartItems.length === 0}
+		<EmptyState
+			title="Your cart is empty"
+			description="Must add some products to cart first before you can checkout"
+		/>
+	{:else}
+		<div class="w-full grid grid-cols-2 gap-x-6 items-start justify-center">
+			<div
+				id="cart-items"
+				class="flex flex-col col-span-2 sm:col-span-1 w-full items-center justify-center gap-3 max-w-2xl"
+			>
 				{#each allCartItems as item}
 					<ProductCard
 						title={item.name}
@@ -247,76 +252,79 @@
 					>
 						<form id="remove-product-from-cart" method="POST" use:enhance>
 							<input hidden type="text" name="id" value={item.cartId} />
-							<button type="submit" formaction="?/removeFromCart" class="text-gray-900 text-sm"
+							<button
+								type="submit"
+								formaction="?/removeFromCart"
+								class="text-gray-900 text-sm hover:text-red-500"
 								>Remove
 							</button>
 						</form>
 					</ProductCard>
 				{/each}
+			</div>
+
+			{#if subtotalPrice > 0}
+				<div id="card-value" class="col-span-2 sm:col-span-1 bg-gray-50 h-screen">
+					<section
+						aria-labelledby="summary-heading"
+						class="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
+					>
+						<h2 id="summary-heading" class="text-lg font-medium text-gray-900">Order summary</h2>
+
+						<dl class="mt-6 space-y-4">
+							<div class="flex items-center justify-between">
+								<dt class="text-sm text-gray-600">Subtotal</dt>
+								<dd class="text-sm font-medium text-gray-900">${subtotalPrice}</dd>
+							</div>
+							<div class="flex items-center justify-between border-t border-gray-200 pt-4">
+								<dt class="flex items-center text-sm text-gray-600">
+									<span>Shipping estimate</span>
+									<a href="#" class="ml-2 flex-shrink-0 text-gray-400 hover:text-gray-500">
+										<span class="sr-only">Learn more about how shipping is calculated</span>
+										<svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+											<path
+												fill-rule="evenodd"
+												d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM8.94 6.94a.75.75 0 11-1.061-1.061 3 3 0 112.871 5.026v.345a.75.75 0 01-1.5 0v-.5c0-.72.57-1.172 1.081-1.287A1.5 1.5 0 108.94 6.94zM10 15a1 1 0 100-2 1 1 0 000 2z"
+												clip-rule="evenodd"
+											/>
+										</svg>
+									</a>
+								</dt>
+								<dd class="text-sm font-medium text-gray-900">$5.00</dd>
+							</div>
+							<div class="flex items-center justify-between border-t border-gray-200 pt-4">
+								<dt class="flex text-sm text-gray-600">
+									<span>Tax estimate</span>
+									<a href="#" class="ml-2 flex-shrink-0 text-gray-400 hover:text-gray-500">
+										<span class="sr-only">Learn more about how tax is calculated</span>
+										<svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+											<path
+												fill-rule="evenodd"
+												d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM8.94 6.94a.75.75 0 11-1.061-1.061 3 3 0 112.871 5.026v.345a.75.75 0 01-1.5 0v-.5c0-.72.57-1.172 1.081-1.287A1.5 1.5 0 108.94 6.94zM10 15a1 1 0 100-2 1 1 0 000 2z"
+												clip-rule="evenodd"
+											/>
+										</svg>
+									</a>
+								</dt>
+								<dd class="text-sm font-medium text-gray-900">$8.32</dd>
+							</div>
+							<div class="flex items-center justify-between border-t border-gray-200 pt-4">
+								<dt class="text-base font-medium text-gray-900">Order total</dt>
+								<dd class="text-base font-medium text-gray-900">${finalPrice}</dd>
+							</div>
+						</dl>
+
+						<div class="mt-6">
+							<button
+								type="button"
+								on:click={handleCheckout}
+								class="w-full rounded-md border border-transparent bg-pink-500 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+								>Checkout</button
+							>
+						</div>
+					</section>
+				</div>
 			{/if}
 		</div>
-
-		{#if subtotalPrice > 0}
-			<div id="card-value" class="col-span-2 sm:col-span-1 bg-gray-50 h-screen">
-				<section
-					aria-labelledby="summary-heading"
-					class="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
-				>
-					<h2 id="summary-heading" class="text-lg font-medium text-gray-900">Order summary</h2>
-
-					<dl class="mt-6 space-y-4">
-						<div class="flex items-center justify-between">
-							<dt class="text-sm text-gray-600">Subtotal</dt>
-							<dd class="text-sm font-medium text-gray-900">${subtotalPrice}</dd>
-						</div>
-						<div class="flex items-center justify-between border-t border-gray-200 pt-4">
-							<dt class="flex items-center text-sm text-gray-600">
-								<span>Shipping estimate</span>
-								<a href="#" class="ml-2 flex-shrink-0 text-gray-400 hover:text-gray-500">
-									<span class="sr-only">Learn more about how shipping is calculated</span>
-									<svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-										<path
-											fill-rule="evenodd"
-											d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM8.94 6.94a.75.75 0 11-1.061-1.061 3 3 0 112.871 5.026v.345a.75.75 0 01-1.5 0v-.5c0-.72.57-1.172 1.081-1.287A1.5 1.5 0 108.94 6.94zM10 15a1 1 0 100-2 1 1 0 000 2z"
-											clip-rule="evenodd"
-										/>
-									</svg>
-								</a>
-							</dt>
-							<dd class="text-sm font-medium text-gray-900">$5.00</dd>
-						</div>
-						<div class="flex items-center justify-between border-t border-gray-200 pt-4">
-							<dt class="flex text-sm text-gray-600">
-								<span>Tax estimate</span>
-								<a href="#" class="ml-2 flex-shrink-0 text-gray-400 hover:text-gray-500">
-									<span class="sr-only">Learn more about how tax is calculated</span>
-									<svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-										<path
-											fill-rule="evenodd"
-											d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM8.94 6.94a.75.75 0 11-1.061-1.061 3 3 0 112.871 5.026v.345a.75.75 0 01-1.5 0v-.5c0-.72.57-1.172 1.081-1.287A1.5 1.5 0 108.94 6.94zM10 15a1 1 0 100-2 1 1 0 000 2z"
-											clip-rule="evenodd"
-										/>
-									</svg>
-								</a>
-							</dt>
-							<dd class="text-sm font-medium text-gray-900">$8.32</dd>
-						</div>
-						<div class="flex items-center justify-between border-t border-gray-200 pt-4">
-							<dt class="text-base font-medium text-gray-900">Order total</dt>
-							<dd class="text-base font-medium text-gray-900">${finalPrice}</dd>
-						</div>
-					</dl>
-
-					<div class="mt-6">
-						<button
-							type="button"
-							on:click={handleCheckout}
-							class="w-full rounded-md border border-transparent bg-pink-500 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
-							>Checkout</button
-						>
-					</div>
-				</section>
-			</div>
-		{/if}
-	</div>
+	{/if}
 </div>
